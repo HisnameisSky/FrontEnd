59bb186b58b7e1fe0974975ee0f381416792a09f
澄界 <hisnameissky@gmail.com>
2025-10-10 17:22:42 +0800

    Add files via upload

---
+++ javascript trivia bot/script.js
@@ -0,0 +1,27 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+// Declare the bot's name, location, and favorite language
+let botName = "Your bot";
+let botLocation = "the cloud";
+let favoriteLanguage = "JavaScript";
+
+// bot's introduction
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+// bot's favorite 
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+// Create the codingFact variable 
+let codingFact = favoriteLanguage + " is one of the most popular languages in the world!";
+console.log(codingFact);
+
+// Reassign the codingFact variable 
+codingFact = favoriteLanguage + " was created in just 10 days by Brendan Eich in 1995.";
+console.log(codingFact);
+
+// Reassign the codingFact variable to another fact 
+codingFact = favoriteLanguage + " is used for both front-end and back-end development.";
+console.log(codingFact);
+
+//farewell message
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
---
+++ sentence maker/script.js
@@ -0,0 +1,22 @@
+let adjective = "fierce";
+let noun = "dragon";
+let verb = "running";
+let place = "mountain";
+let adjective2 = "smelly";
+let noun2 = "cheese";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+
+console.log("First story: " + firstStory);
+
+adjective = "small";
+noun = "mouse";
+verb = "jumping";
+place = "cottage";
+adjective2 = "clean";
+noun2 = "apple";
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: "+ secondStory);
